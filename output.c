d6f3e55d7f1c1e493cc6ce431fcadba82906e049
guneetkaur101 <75029108+guneetkaur101@users.noreply.github.com>
2021-10-07 00:35:13 +0530

    output of Covid-19 report

---
+++ output.c
@@ -0,0 +1,55 @@
+ Enter your Personal information::
+Patient name:Guneet
+Father's name/Husband's name:ABC
+Gender:female
+Age:22
+Enter 1 for CORONA TEST
+
+1
+*****************************************
+                COVID-19
+*****************************************
+
+Lets Begins
+
+Please answer the following Questions:
+Press 1 for YES and 0 for NO
+
+Does patient have a Fever
+0
+Does patient have Fever last week
+1
+Does patient have a Headache
+1
+Does patient have a Running Nose
+0
+Does patient have a Dry cough
+1
+Does patient have a Body ache
+1
+Does patient loss of taste and smell
+0
+Does patient have a sore throat
+1
+Does patient fell difficulty in Breathing
+0
+Does patient feel Chest pain
+0
+*******************************
+*******************************
+-----------REPORT--------------
+*******************************
+*******************************
+
+Patient name:Guneet
+Father's name/ Husband's name:ABC
+Gender:female
+Age:22
+
+The patient have symptoms of covid-19
+Which indicates it's POSITIVE CASE, Take care
+
+THANK YOU AND HAVE A NICE DAY
+
+Process returned 0 (0x0)   execution time : 36.015 s
+Press any key to continue.
